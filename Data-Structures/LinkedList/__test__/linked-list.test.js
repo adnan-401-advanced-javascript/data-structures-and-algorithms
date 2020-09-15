5280b9a8071da2c30896033bde5fa48c552a9138
amuammer <amuammer@students.iugaza.edu.ps>
2020-09-15 06:00:45 +0300

    adding cc06

--- Data-Structures/LinkedList/__test__/linked-list.test.js
+++ Data-Structures/LinkedList/__test__/linked-list.test.js
@@ -1,7 +1,6 @@
 const LinkedList = require("../linked-list.js");
 
 describe('linked list module', () => {
-
         it('test an empty LinkedList', () => {
           const newLinkedList = new LinkedList();
           expect(newLinkedList.head).toBe(null);
@@ -70,6 +69,71 @@ describe('linked list module', () => {
           const value2 = "adnan2";
           newLinkedList.insert(value2);
           expect(newLinkedList.toString()).toBe("{adnan1} -> {adnan2} -> NULL");
+        });
+
+        it('test append', () => {
+          const newLinkedList = new LinkedList();
+          const value = "adnan";
+          newLinkedList.append(value);
+          expect(newLinkedList.head.value === value);
+          expect(newLinkedList.head.next).toBe(null);
+        });
+
+        it('test insertBefore an empty', () => {
+          const newLinkedList = new LinkedList();
+          const value = "adnan";
+          try {
+            newLinkedList.insertBefore(1, value);
+          } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+           expect(error).toHaveProperty('message', '1 not found');
+          }
+        });
+
+        it('test insertBefore an element', () => {
+          const newLinkedList = new LinkedList();
+          const value = "adnan";
+            newLinkedList.append(1);
+            newLinkedList.insertBefore(1, value);
+            expect(newLinkedList.head.value === value);
+            expect(newLinkedList.head.next).not.toBe(null);
+            expect(newLinkedList.head.next.value).toBe(1);
+        });
+
+
+        it('test insertBefore in the middle', () => {
+          const newLinkedList = new LinkedList();
+          const value = "adnan";
+            newLinkedList.append(1);
+            newLinkedList.append(2);
+            newLinkedList.insertBefore(2, value);
+            expect(newLinkedList.head.value === 1);
+            expect(newLinkedList.head.next.value === value);
+           expect(newLinkedList.head.next.next.value).toBe(2);
+           expect(newLinkedList.head.next.next.next).toBe(null);
+        });
+
+        it('test insertAfter not found', () => {
+          const newLinkedList = new LinkedList();
+          const value = "adnan";
+          try {
+            newLinkedList.insertAfter(1, value);
+          } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+           expect(error).toHaveProperty('message', '1 not found');
+          }
+        });
+
+        it('test insertAfter two elements', () => {
+          const newLinkedList = new LinkedList();
+          const value = "adnan";
+          newLinkedList.append(1);
+          newLinkedList.append(2);
+          newLinkedList.insertAfter(2, value);
+          expect(newLinkedList.head.value === 1);
+          expect(newLinkedList.head.next.value === 2);
+          expect(newLinkedList.head.next.next.value).toBe(value);
+          expect(newLinkedList.head.next.next.next).toBe(null);
         });
 
 
--- Data-Structures/LinkedList/linked-list.js
+++ Data-Structures/LinkedList/linked-list.js
@@ -65,6 +65,65 @@ class LinkedList {
     }
     return result;
   }
+
+  /**
+   * [append -> insert at the tail]
+   * @method append
+   * @param  {*} value
+   * @return {LinkedList}
+   */
+  append(value) {
+    return this.insert(value);
+  }
+
+  /**
+   * [insertBefore]
+   * @method insertBefore
+   * @param  {*}  value
+   * @param  {*}  newValue
+   * @return {LinkedList}
+   */
+  insertBefore(value, newValue) {
+    const newNode = new Node(newValue);
+    let currentNode = this.head;
+    let prev;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        if (!prev) {
+          newNode.next = currentNode;
+          this.head = newNode;
+          return this;
+        }
+        newNode.next = currentNode;
+        prev.next = newNode;
+        return this;
+      }
+      prev = currentNode;
+      currentNode = currentNode.next;
+    }
+    throw new Error(`${value} not found`);
+  }
+
+  /**
+   * [insertAfter description]
+   * @method insertAfter
+   * @param  {*}    value
+   * @param  {*}    newValue
+   * @return {LinkedList}
+   */
+  insertAfter(value, newValue) {
+    const newNode = new Node(newValue);
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+        return this;
+      }
+      currentNode = currentNode.next;
+    }
+    throw new Error(`${value} not found`);
+  }
 }
 
 module.exports = LinkedList;
